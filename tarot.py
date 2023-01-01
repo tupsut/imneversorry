d42dc5a2d727ad9651d550a3c3155ea002623ff1
tupsu <tupsu+gth@tupsu.eu>
2023-01-01 19:27:06 +0200

    refactor tarot.py for clarity, add väärinpäin as keyword

--- tarot.py
+++ tarot.py
@@ -8,37 +8,20 @@ import db
 import re
 
 
-
 class Tarot:
     def __init__(self):
         self.card_data = db.readSelitykset()
-    
-    def getCommands(self):
-        return dict()
-
-    def get_reading(self, amount):
-        # cards in resources folder
-        cards = listdir("resources/tarot")
-        # magic shuffling
-        shuffle(cards)
-        reading = []
-        for i in range(amount):
-            # how 2 reverse a queue
-            reading.append(cards.pop())
-
-        # return the tempfile with the image
-        return(self.make_image(reading))
 
     def make_image(self, reading):
         reading_image = Image.new('RGB', (250 * len(reading), 429))
 
         for i in range(len(reading)):
             # chance for flipped card
-            if randint(0,10) == 0:
+            if randint(0, 10) == 0:
                 card_image = Image.open("resources/tarot/" + reading[i])
                 image_flipped = card_image.transpose(Image.FLIP_TOP_BOTTOM)
                 reading_image.paste(im=image_flipped, box=(250 * i, 0))
-            #normal card
+            # normal card
             else:
                 reading_image.paste(im=Image.open("resources/tarot/" + reading[i]), box=(250 * i, 0))
 
@@ -48,35 +31,35 @@ class Tarot:
         fp = NamedTemporaryFile()
         fp.seek(0)
         reading_image.save(fp, 'jpeg', quality=75)
-        return(fp)
+        return fp
 
-    def explain_card(self, text):
-        explanations_to_return = ""
+    def draw_cards(self, amount):
+        # cards in resources folder
+        cards = listdir("resources/tarot")
+        # magic shuffling
+        shuffle(cards)
+        # add spookiness as requested, shuffle more
+        shuffle(cards)
+        shuffle(cards)
+        reading = []
+        for i in range(amount):
+            # how 2 reverse a queue
+            reading.append(cards.pop())
 
-        for datum in self.card_data:
-            name = datum[0]
-            lname = name.lower()
-            if lname in text:
-                if "reversed " + lname in text or "ylösalaisin " + lname in text or lname + " reversed" in text or lname + " ylösalaisin" in text:
-                    rev_exp = datum[2]
-                    explanations_to_return += "Reversed " + name + ": " + rev_exp + "\n\n"
-                    continue
-                explanation = datum[1]
-                explanations_to_return += name + ": " + explanation + "\n\n"
+        # return the temp file with the image
+        return self.make_image(reading)
 
-        return explanations_to_return
-
-    def getTarot(self, update: Update, context: CallbackContext):
+    def get_reading(self, update: Update, context: CallbackContext):
         try:
             size = int(update.message.text.lower().split(' ')[1])
-        except ValueError :
+        except ValueError:
             context.bot.sendMessage(chat_id=update.message.chat_id, text=":--D")
             return
 
         if size < 1 or size > 78:
             context.bot.sendMessage(chat_id=update.message.chat_id, text=":--D")
             return
-        image_file = self.get_reading(size)
+        image_file = self.draw_cards(size)
         image_file.seek(0)
         if size > 10:
             context.bot.sendDocument(chat_id=update.message.chat_id, document=open(image_file.name, 'rb'))
@@ -84,15 +67,35 @@ class Tarot:
             context.bot.send_photo(chat_id=update.message.chat_id, photo=open(image_file.name, 'rb'))
         image_file.close()
 
-    def getReading(self, update: Update, context: CallbackContext):
+    def explain_card(self, text):
+        explanations = ""
+
+        for datum in self.card_data:
+            name = datum[0].lower()
+            if name in text:
+                for keyword in ["reversed", "ylösalaisin", "väärinpäin"]:
+                    if keyword + " " + name in text or name + " " + keyword in text:
+                        rev_exp = datum[2]
+                        explanations += "Reversed " + name + ": " + rev_exp + "\n\n"
+                        continue
+                explanation = datum[1]
+                explanations += name + ": " + explanation + "\n\n"
+
+        return explanations
+
+    def get_explanation(self, update: Update, context: CallbackContext):
         message = self.explain_card(update.message.text.lower())
         if message != "":
             context.bot.sendMessage(chat_id=update.message.chat_id, text=message)
 
     def messageHandler(self, update: Update, context: CallbackContext):
         msg = update.message
-        if msg.text is not None:
-            if re.match(r'^/tarot [0-9]+(?!\S)', msg.text.lower()):
-                self.getTarot(update, context)
-            elif "selitä" in msg.text.lower() or "selitys" in msg.text.lower():
-                self.getReading(update, context)
+        text = msg.text.lower()
+        if text is not None:
+            if re.match(r'^/tarot [0-9]+(?!\S)', text):
+                self.get_reading(update, context)
+            elif "selitä" in text or "selitys" in text:
+                self.get_explanation(update, context)
+
+    def getCommands(self):
+        return dict()
